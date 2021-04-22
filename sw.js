fb86fbe513b22567a4f051eb4d5b024e8579b1b0
Eric Schulte <eschulte@grammatech.com>
2021-04-22 19:20:52 -0400

    Drop unnecessary logging

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-console.log('Script loaded!')
 var cacheStorageKey = 'macros'
 
 var cacheList = [
@@ -25,27 +24,21 @@ var cacheList = [
 ]
 
 self.addEventListener('install', function(e) {
-  console.log('Cache event!')
   e.waitUntil(
     caches.open(cacheStorageKey).then(function(cache) {
-      console.log('Adding to Cache:', cacheList)
       return cache.addAll(cacheList)
     }).then(function() {
-      console.log('Skip waiting!')
       return self.skipWaiting()
     })
   )
 })
 
 self.addEventListener('fetch', function(e) {
-  console.log('Fetch event:', e.request.url)
   e.respondWith(
     caches.match(e.request).then(function(response) {
       if (response != null) {
-        console.log('Using cache for:', e.request.url)
         return response
       }
-      console.log('Fallback to fetch:', e.request.url)
       return fetch(e.request.url)
     })
   )
